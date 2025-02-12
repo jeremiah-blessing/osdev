07d66bcabf3830e439cf67be86f86e6c27dfb73d
Jeremiah Blessing <vjeremiahblessing@gmail.com>
2025-02-12 23:59:18 +0530

    Add syntax highlighting configuration for Markdown in Astro

--- documentation/astro.config.mjs
+++ documentation/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	markdown : {syntaxHighlight  :"prism"},
 	integrations: [
 		starlight({
 			title: 'OSDev',
